f91d16171aaea2a5a7e08792f7820cfae8cd3c3d
katonada <davor.katona.sesto@wm.rs>
2023-07-16 18:24:17 +0200

    eslint added

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import { PropTypes } from 'prop-types';
+import Button from './Button';
+
+function Header({ title }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Button color="green" text="Hello green" />
+      <Button color="red" text="Hello red" />
+      <Button color="blue" text="Hello blue" />
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
+
+export default Header;
