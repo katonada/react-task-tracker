82362f05166c43aa62a826ca1ad38f5c539aa1eb
katonada <davor.katona.sesto@wm.rs>
2023-07-17 09:07:38 +0200

    add node 18 instaation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="container">
-      <Header _title="Hello" />
+      <Header title="Hello" />
     </div>
   );
 }
