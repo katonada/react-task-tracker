93287a190a1b6f5b162d17ff24d8b83d6c39f316
katonada <davor.katona.sesto@wm.rs>
2023-07-19 08:20:01 +0200

    delete task

--- src/App.js
+++ src/App.js
@@ -5,30 +5,37 @@ import Tasks from "./components/Tasks";
 function App() {
 
 const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctor',
-            day: "05. Feb at 8:00",
-            reminder: true
-        },
-        {
-            id: 2,
-            text: 'School meeting',
-            day: "06. Feb at 14:45",
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'Shopping',
-            day: "05. Feb at 17:00",
-            reminder: false
-        }
-    ])
+      {
+          id: 1,
+          text: 'Doctor',
+          day: "05. Feb at 8:00",
+          reminder: true
+      },
+      {
+          id: 2,
+          text: 'School meeting',
+          day: "06. Feb at 14:45",
+          reminder: true
+      },
+      {
+          id: 3,
+          text: 'Shopping',
+          day: "05. Feb at 17:00",
+          reminder: false
+      }
+  ])
+
+  const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (
+
     <div className="container">
       <Header title="Hello" />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0
+        ? <Tasks tasks={tasks} onDelete={deleteTask}/>
+      : 'No tasks'}
     </div>
   );
 }
