4c61caee37dc445bdb5aacdc1cd82048777b5e1f
katonada <davor.katona.sesto@wm.rs>
2023-07-21 18:27:57 +0200

    add DELETE; PUT; POST fetch methods

--- src/App.js
+++ src/App.js
@@ -19,24 +19,58 @@ function App() {
   }, [])
 
   const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks')
-      const data = await res.json()
-      console.log({data});
-      return data
-    }
-
-  const addTask = (task) => {
-      const id = Math.floor(Math.random() * 1000000) + 1
-      const newTask = {id, ...task}
-      setTasks([...tasks, newTask])
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
   }
 
-  const deleteTask = (id) => {
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
+  const addTask = async (task) => {
+
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
+  }
+
+  const deleteTask = async (id) => {
+      await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'DELETE'
+      })
       setTasks(tasks.filter((task) => task.id !== id))
   }
 
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = res.json()
+
+    setTasks(
+      tasks.map((task) => task.id === id
+        ? {...task, reminder: data.reminder}
+        : task)
+    )
   }
 
   return (
